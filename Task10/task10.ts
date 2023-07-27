f9b2fa134cb786d334c090bc83b301785d958bf9
Sheeza Nasar <139039450+sheezanasar@users.noreply.github.com>
2023-07-27 14:41:42 +0500

    task10.ts

--- Task10/task10.ts
+++ Task10/task10.ts
@@ -1,17 +1,31 @@
-let girlName = "Sheeza";
-console.log("Hello, " + girlName + "! Welcome to the program.");
+//------------------------------
+//The program defines a famous_person and a quote using variables.
+{
+  // Block 1
+  // Declare variables to store the famous person's name and a quote by them.
+  let famous_person = "Maulana Rumi";
+  let quote = "Live life as if everything is rigged in your favour.";
 
-function generateFibonacciSequence(n: number): number[] {
-    let sequence: number[] = [0, 1];
-  
-    for (let i = 2; i < n; i++) {
-      sequence[i] = sequence[i - 1] + sequence[i - 2];
-    }
-  
-    return sequence;
-  }
-  
-  let limit: number = 10;
-  let fibonacciSequence: number[] = generateFibonacciSequence(limit);
-  console.log("Fibonacci Sequence up to " + limit + ":", fibonacciSequence);
+  // Create a message using string interpolation that includes the famous person's name and the quote.
+  let message = `${famous_person} once said, ${quote}`;
+
+  // Output the message to the console.
+  console.log(message);
+}
+
+{
+  // Block 2
+  // The same code as Block 1 is repeated here.
+
+  // Declare variables to store the famous person's name and a quote by them.
+  let famous_person = "Maulana Rumi";
+  let quote = "Live life as if everything is rigged in your favour.";
+
+  // Create a message using string interpolation that includes the famous person's name and the quote.
+  let message = `${famous_person} once said, ${quote}`;
+
+  // Output the message to the console.
+  console.log(message);
+}
+
   
