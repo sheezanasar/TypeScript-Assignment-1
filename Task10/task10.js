8cc3c757880c84a73d527265aecfceb3ef28e212
Sheeza Nasar <139039450+sheezanasar@users.noreply.github.com>
2023-07-27 14:43:40 +0500

    task10.js

--- Task10/task10.js
+++ Task10/task10.js
@@ -1,13 +1,24 @@
 "use strict";
-let girlName = "Sheeza";
-console.log("Hello, " + girlName + "! Welcome to the program.");
-function generateFibonacciSequence(n) {
-    let sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-        sequence[i] = sequence[i - 1] + sequence[i - 2];
-    }
-    return sequence;
+//------------------------------
+//The program defines a famous_person and a quote using variables.
+{
+    // Block 1
+    // Declare variables to store the famous person's name and a quote by them.
+    let famous_person = "Maulana Rumi";
+    let quote = "Live life as if everything is rigged in your favour.";
+    // Create a message using string interpolation that includes the famous person's name and the quote.
+    let message = `${famous_person} once said, ${quote}`;
+    // Output the message to the console.
+    console.log(message);
 }
-let limit = 10;
-let fibonacciSequence = generateFibonacciSequence(limit);
-console.log("Fibonacci Sequence up to " + limit + ":", fibonacciSequence);
+{
+    // Block 2
+    // The same code as Block 1 is repeated here.
+    // Declare variables to store the famous person's name and a quote by them.
+    let famous_person = "Maulana Rumi";
+    let quote = "Live life as if everything is rigged in your favour.";
+    // Create a message using string interpolation that includes the famous person's name and the quote.
+    let message = `${famous_person} once said, ${quote}`;
+    // Output the message to the console.
+    console.log(message);
+}
